3fd3f305ac5b081d98743c9360acb0091fb696ee
Kristian Dorland <kdo@kvalitetsit.dk>
2025-09-25 15:42:48 +0200

    IUAKT-81: Mere tests og simplere dsl-pipe.

--- itukt-ui/src/app/features/clauses/clauses.ts
+++ itukt-ui/src/app/features/clauses/clauses.ts
@@ -1,14 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ClausesService } from '../../services/clauses';
-import { ClauseOutput } from '@api/models/clause-output'; 
 import { DslHighlightPipe } from '../../shared/dsl-highlight-pipe';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { DslOutput } from '@api/models/dsl-output';
 
-// TODO: Der mangler tests.
-// TODO: Vi bør skifte til at bruge https://material.angular.dev/components/table/
+// TODO: Det kunne være en idé at skifte til at bruge: https://material.angular.dev/components/table/
 @Component({
   selector: 'app-clauses',
   standalone: true,
@@ -19,17 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
 export class Clauses{
   private service = inject(ClausesService);
 
-  // Signals i stedet for async pipe (som i gamle dage)
-  clauses = toSignal<ClauseOutput[]>(
-    this.service.getClauses()
+  clauses = toSignal<Array<DslOutput>>(
+    this.service.getClausesAsDsl()
   );
-
-  // TODO: Skift til at hente DSL direkte måske?
-  summarize(expr: any): string {
-    if (!expr) return '';
-    if (expr.type === 'StringCondition') return `${expr.field} = "${expr.value}"`;
-    if (expr.type === 'NumberCondition') return `${expr.field} ${expr.operator} ${expr.value}`;
-    if (expr.type === 'BinaryExpression') return `(${this.summarize(expr.left)}) ${expr.operator} (${this.summarize(expr.right)})`;
-    return '';
-  }
 }
--- itukt-ui/src/app/services/clauses.ts
+++ itukt-ui/src/app/services/clauses.ts
@@ -2,14 +2,17 @@ import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ManagementService } from '@api/services/management.service';
 import { ClauseOutput } from '@api/models/clause-output'; 
+import { DslOutput } from '@api/models/dsl-output';
 
 @Injectable({ providedIn: 'root' })
 export class ClausesService {
   private api = inject(ManagementService);
 
-  // Hent alle clauses
-  getClauses(): Observable<ClauseOutput[]> {
-    // Her anvendes default-udgave der blot returnerer 'body'
+  getClausesAsJson(): Observable<ClauseOutput[]> {
     return this.api.getAllClausesV20250801$Json();
+  }
+
+  getClausesAsDsl(): Observable<Array<DslOutput>> {
+    return this.api.getAllClausesDslV20250801();
   }
 }
