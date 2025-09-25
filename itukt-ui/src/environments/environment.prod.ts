7d4e67454bbd5279a3b99f1d3b63f342c37a13d5
Kristian Dorland <kdo@kvalitetsit.dk>
2025-09-25 14:07:11 +0200

    IUAKT-81: Env vars til API URL.

--- itukt-ui/src/environments/environment.prod.ts
+++ itukt-ui/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  // TODO: Inject this during build time
-  apiBaseUrl: 'http://localhost:8080'
+  // Brug værdien der sættes via container-start, med fallback til localhost:8080
+  apiBaseUrl: (window as any)?.__RUNTIME_CONFIG__?.ITUKT_API_BASE_URL 
+              ?? 'http://localhost:8080'
 };
