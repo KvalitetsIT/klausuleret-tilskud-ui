b43d8b8674c42193b2cf4d9341904a9204609534
Kristian Dorland <kdo@kvalitetsit.dk>
2025-10-01 10:28:42 +0200

    IUAKT-81: Fjerner hello :)

--- itukt-ui/src/app/app.routes.ts
+++ itukt-ui/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Hello} from './features/hello/hello';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'hello', component: Hello }
+  { path: '', component: Home }
 ];
