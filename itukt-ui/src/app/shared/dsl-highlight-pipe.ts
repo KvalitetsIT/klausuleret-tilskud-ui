b3f231d38f317071d2a58fd500ea4801c817d8df
Kristian Dorland <kdo@kvalitetsit.dk>
2025-10-01 10:15:19 +0200

    IUAKT-81: Simplere CSS til syntax highlighting

--- itukt-ui/src/app/shared/dsl-highlight-pipe.ts
+++ itukt-ui/src/app/shared/dsl-highlight-pipe.ts
@@ -11,19 +11,19 @@ export class DslHighlightPipe implements PipeTransform {
     let out = src;
 
     // operators
-    out = out.replace(/(>=|<=|=|>|<)/gi, '<span class="op">$1</span>');
+    out = out.replace(/(>=|<=|=|>|<)/gi, '<span class="dsl-operator">$1</span>');
 
     // keywords
-    out = out.replace(/\b(eller|og)\b/g, '<span class="k">$1</span>');
+    out = out.replace(/\b(eller|og)\b/g, '<span class="dsl-keyword">$1</span>');
 
     // numbers
-    out = out.replace(/\b\d+(?:\.\d+)?\b/g, '<span class="num">$&</span>');
+    out = out.replace(/\b\d+(?:\.\d+)?\b/g, '<span class="dsl-number">$&</span>');
 
     // identifiers
-    out = out.replace(/\b[A-Z][A-Z0-9_]*\b/g, '<span class="id">$&</span>');
+    out = out.replace(/\b[A-Z][A-Z0-9_]*\b/g, '<span class="dsl-id">$&</span>');
 
     // punctuation
-    out = out.replace(/[:(),]/g, '<span class="p">$&</span>');
+    out = out.replace(/[:(),]/g, '<span class="dsl-punctuation">$&</span>');
 
     return out;
   }
