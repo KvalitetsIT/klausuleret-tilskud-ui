f8a98481927f2f83b607331f6c2af7481b086ae5
Thomas Lange <tsl@kvalitetsit.dk>
2025-12-09 14:11:04 +0100

    Show tables with mui table

--- itukt-ui/src/app/features/clauses/clauses.ts
+++ itukt-ui/src/app/features/clauses/clauses.ts
@@ -6,19 +6,22 @@ import { DslHighlightPipe } from '../../shared/dsl-highlight-pipe';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { DslOutput } from '@api/model/dslOutput';
+import { MatTableModule } from '@angular/material/table';
 
 // TODO: Det kunne være en idé at skifte til at bruge: https://material.angular.dev/components/table/
 @Component({
   selector: 'app-clauses',
   standalone: true,
-  imports: [DslHighlightPipe, MatCardModule, MatIconModule],
+  imports: [DslHighlightPipe, MatCardModule, MatIconModule, MatTableModule],
   templateUrl: 'clauses.html',
   styleUrls: ['clauses.css']
 })
-export class Clauses{
+export class Clauses {
   private service = inject(ClausesService);
 
   clauses = toSignal<Array<DslOutput>>(
     this.service.getClausesAsDsl()
   );
+
+  displayedColumns: string[] = ['dsl', 'error'];
 }
