66bffb2ae7b5a6ce3c7c770be99ecf713a9222ea
Thomas Lange <tsl@kvalitetsit.dk>
2025-12-16 11:21:39 +0100

    Update title

--- itukt-ui/src/app/pages/home/home.ts
+++ itukt-ui/src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { Clauses } from '../../features/clauses/clauses';
 import { App } from '../../app';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Clauses, MatCardModule],
+  imports: [Clauses, MatCardModule, MatToolbarModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
