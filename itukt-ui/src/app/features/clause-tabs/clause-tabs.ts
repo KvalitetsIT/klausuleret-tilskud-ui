1a7b3d2ecd3f9e09c576bea227f28b5e1319b68a
Thomas Lange <tsl@kvalitetsit.dk>
2025-12-17 11:48:08 +0100

    Add create clause button and dialog

--- itukt-ui/src/app/features/clause-tabs/clause-tabs.ts
+++ itukt-ui/src/app/features/clause-tabs/clause-tabs.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Clauses } from '../clauses/clauses';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateClauseDialog } from '../create-clause-dialog/create-clause-dialog';
 
 @Component({
   selector: 'app-clause-tabs',
   standalone: true,
-  imports: [Clauses, MatCardModule, MatTabsModule],
+  imports: [Clauses, MatCardModule, MatTabsModule, MatIconModule, MatButtonModule],
   templateUrl: 'clause-tabs.html',
   styleUrls: ['clause-tabs.css']
 })
 export class ClauseTabs {
+  dialog = inject(MatDialog);
+
+  openCreateClauseDialog(): void {
+    const dialogRef = this.dialog.open(CreateClauseDialog, {
+      data: { dsl: "", error: "" },
+      minWidth: '700px'
+    });
+  }
 }
