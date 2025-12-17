03c2c50bfe25a72c25e1cf1d009195904979508e
Thomas Lange <tsl@kvalitetsit.dk>
2025-12-17 16:04:42 +0100

    Show snackbar on success and failure

--- itukt-ui/src/app/services/clauses.ts
+++ itukt-ui/src/app/services/clauses.ts
@@ -4,16 +4,38 @@ import { Observable } from 'rxjs';
 import { ManagementService } from '@api/api/management.service';
 import { ClauseStatus, DslInput } from '@api/index';
 import { DslOutput } from '@api/model/dslOutput';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class ClausesService {
   private api = inject(ManagementService);
+  private snackbar = inject(MatSnackBar);
 
   getClauses(status: ClauseStatus): Observable<Array<DslOutput>> {
     return this.api.getAllClausesDslV20250801(status);
   }
 
   createClause(dslInput: DslInput): Observable<DslOutput> {
-    return this.api.createClauseFromDslV20250801(dslInput);
+    const response = this.api.createClauseFromDslV20250801(dslInput);
+    this.subscribeWithSnackbar(response, "Klausul blev oprettet", "Klausul oprettelse fejlede");
+    return response;
+  }
+
+  subscribeWithSnackbar(response: Observable<any>, successMessage: string, errorMessage: string): void {
+    response.subscribe({
+      next: (result) => {
+        this.openSnackbar(successMessage);
+      },
+      error: (err) => {
+        this.openSnackbar(errorMessage + '.\n' + err.error.message);
+      }
+    });
+  }
+
+  openSnackbar(message: string): void {
+    this.snackbar.open(message, 'Close', {
+      duration: 5000,
+      panelClass: 'snackbar'
+    });
   }
 }
