8a61defccedabb115769e015a685169ad49cecc2
Thomas Lange <tsl@kvalitetsit.dk>
2026-01-06 11:52:12 +0100

    Add clause dialog

---
+++ itukt-ui/src/app/features/clause-dialog/clause-dialog.ts
@@ -0,0 +1,26 @@
+import { DatePipe } from "@angular/common";
+import { Component, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle } from "@angular/material/dialog";
+import { MatListModule } from "@angular/material/list";
+import { DslOutput } from "@api/index";
+import { DslHighlightPipe } from "src/app/shared/dsl-highlight-pipe";
+
+@Component({
+    selector: 'clause-dialog',
+    templateUrl: 'clause-dialog.html',
+    styleUrls: ['clause-dialog.css'],
+    imports: [
+        MatDialogTitle,
+        MatDialogContent,
+        MatListModule,
+        DslHighlightPipe,
+        DatePipe
+    ],
+})
+export class ClauseDialog {
+  clause: DslOutput;
+
+  constructor(@Inject(MAT_DIALOG_DATA) data: { clause: DslOutput }) {
+    this.clause = data.clause;
+  }
+}
