14a3dc65dae1324e4f31fea11449605a86dbb4a3
Thomas Lange <tsl@kvalitetsit.dk>
2026-06-30 12:55:02 +0200

    Show drugs count

--- itukt-ui/src/app/features/clause-info/content-items/read/clause-read-items.ts
+++ itukt-ui/src/app/features/clause-info/content-items/read/clause-read-items.ts
@@ -2,13 +2,14 @@ import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { DslOutput } from '@api/index';
+import { DrugsCountChip } from 'src/app/shared/drugs-count-chip/drugs-count-chip';
 import { DslHighlightPipe } from 'src/app/shared/dsl-highlight-pipe';
 
 @Component({
     selector: 'clause-read-items',
     templateUrl: 'clause-read-items.html',
     styleUrls: ['../clause-content-items.css'],
-    imports: [MatListModule, DslHighlightPipe, DatePipe],
+    imports: [MatListModule, DslHighlightPipe, DatePipe, DrugsCountChip],
 })
 export class ClauseReadItems {
     @Input({ required: true }) clause!: DslOutput;
