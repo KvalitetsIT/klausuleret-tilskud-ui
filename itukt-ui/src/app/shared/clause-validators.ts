eae612cfaa522d9e788aebaed77c8e1c2e914bf2
Thomas Lange <tsl@kvalitetsit.dk>
2026-09-02 14:55:40 +0200

    Ignore case when checking if clause name is in use

--- itukt-ui/src/app/shared/clause-validators.ts
+++ itukt-ui/src/app/shared/clause-validators.ts
@@ -15,9 +15,9 @@ export class ClauseValidators {
         this.clauseService.getClauses(ClauseStatus.Draft)
     );
 
-    clauseNameInUseValidator = (control: AbstractControl): ValidationErrors | null => {
-        return this.activeClauses()?.some(c => c.name === control.value)
-            || this.draftClauses()?.some(c => c.name === control.value)
+    clauseNameInUseValidator = (control: AbstractControl<string>): ValidationErrors | null => {
+        return this.activeClauses()?.some(c => c.name.toUpperCase() === control.value.toUpperCase())
+            || this.draftClauses()?.some(c => c.name.toUpperCase() === control.value.toUpperCase())
             ? { nameInUseByActiveClause: true }
             : null;
     };
